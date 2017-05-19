4a9349287d270bd19f58c8405c100267d7761b26
Javier Panchi <javier.panchi@roames.com.au>
2017-05-20 09:01:30 +1000

    Auth0 Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [ AuthService ],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(private auth: AuthService){
+    this.auth.handleAuthentication();
+
+  }
 }
